d510f978a46d03a53f4a4ed41e13ee24dfbc2e4f
István Káré <istvan.kare@moon42.com>
2024-06-14 11:00:03 +0200

    2.4 Control flow statements

--- src/main/java/com/gde/spring/JavaSpringApplication.java
+++ src/main/java/com/gde/spring/JavaSpringApplication.java
@@ -8,29 +8,22 @@ public class JavaSpringApplication {
      */
     public static void main(String[] args) {
 
-        //Basic data types needed for everyday use of Java, brief explanation for each of them
-        int a = 5;
-        double b = 4.5;
-        float c = 2.333f;
-        boolean d = true;
-        char e = '!';
-        String f = "Hello";
+        // Explain comparison operators, <, >, <=, >=, !, ==, !=, &&, ^
+        int num = 10;
+        if (num == 5) {
+            System.out.println("Exactly 5");
+        } else if(num < 5) {
+            System.out.println("Less than 5");
+        } else {
+            System.out.println("Greater than 5");
+        }
 
-
-        // Basic operations and expressions again explanation of the operators, mention '%' operator
-        String concatenated = f+e;
-        System.out.println(concatenated);
-
-        int x = 5;
-        int y = 12;
-        int sum = x + y;
-        System.out.println("Sum: " + sum);
-
-        // Can go into details on what's the difference between sum++ and ++sum
-        sum++;
-        System.out.println("Sum+1: " + sum);
-
-        int divideRemainder = y % x;
-        System.out.println("Remainder: " + divideRemainder);
+        switch (num) {
+            case 10:
+                System.out.println("Ten");
+                break;
+            default:
+                System.out.println("Not Ten");
+        }
     }
 }
